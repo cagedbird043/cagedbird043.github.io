0cd054498b4a3251a8d986ac3bc4a4e4662dc7f3
cagedbird043 <cagedbird043@gmail.com>
2024-10-15 19:47:55 +0800

    Site updated: 2024-10-15 19:47:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/06/在手机上打造随身开发环境：Termux与code-server的完美结合/"];function toRandomPost(){
+var posts=["2024/10/15/利用termux实现免root的code-server/","2024/10/06/在手机上打造随身开发环境：Termux与code-server的完美结合/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
